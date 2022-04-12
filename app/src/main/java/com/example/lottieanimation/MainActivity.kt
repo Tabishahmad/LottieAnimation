0abf7123ba8dd17d73b73d5825b5c4f31186b452
Tabish Ahmad <tabishahmad1@gmail.com>
2022-04-12 14:06:14 +0530

    LottieAnimation

--- app/src/main/java/com/example/lottieanimation/MainActivity.kt
+++ app/src/main/java/com/example/lottieanimation/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        //Test one
         binding.anim.playAnimation()
 
         binding.anim.addAnimatorListener(object :Animator.AnimatorListener{
